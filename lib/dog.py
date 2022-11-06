52d33c1461ddbcf4e71945cc8d5afd91eda0488d
Laura Berge <lauraeberge@gmail.com>
2022-11-06 09:50:54 -0600

    add solution

--- lib/dog.py
+++ lib/dog.py
@@ -5,4 +5,108 @@ CURSOR = CONN.cursor()
 
 class Dog:
     
-    pass
+    def __init__(self, name, breed, id=None):
+        self.name = name
+        self.breed = breed
+        self.id = id
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = 'DROP TABLE IF EXISTS dogs'
+
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO dogs (name, breed)
+            VALUES (?,?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.breed))
+        self.id = CURSOR.lastrowid
+    
+    @classmethod
+    def create(cls, name, breed):
+        dog = cls(name, breed)
+        dog.save()
+        return dog
+
+    @classmethod
+    def new_from_db(cls, row):
+        return cls(row[1], row[2], row[0])
+
+    
+    @classmethod
+    def get_all(cls):
+        sql = "SELECT * FROM dogs"
+        return [cls.new_from_db(row) for row in CURSOR.execute(sql).fetchall()]
+
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT * FROM dogs
+            WHERE name=?
+            LIMIT 1
+        """
+
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        if not row:
+            return None
+
+        return cls(row[1], row[2], row[0])
+    
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT * FROM dogs
+            WHERE id=?
+            LIMIT 1
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        if not row:
+            return None
+
+        return cls(row[1], row[2], row[0])
+
+    @classmethod
+    def find_or_create_by(cls, name, breed):
+        sql = """
+            SELECT * FROM dogs
+            WHERE (name, breed) = (?, ?)
+            LIMIT 1
+        """
+
+        row = CURSOR.execute(sql, (name, breed)).fetchone()
+        if not row:
+            sql = """
+                INSERT INTO dogs (name, breed)
+                VALUES (?, ?)
+            """
+
+            CURSOR.execute(sql, (name, breed))
+            return cls(name, breed, CURSOR.lastrowid)
+
+        return cls(row[1], row[2], row[0])
+
+    def update(self):
+        sql = """
+            UPDATE dogs
+            SET name = ?,
+                breed = ?
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.name, self.breed, self.id))
